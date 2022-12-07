f02d3d4d58921b13096726f5f297fab73ebc5701
Paul Draper <paul@helloseer.com>
2022-12-07 06:19:29 +0000

    Day 7

---
+++ problems/day-07/part_1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import collections
+import sys
+
+sizes = collections.defaultdict(int)
+line = next(sys.stdin, None)
+while line:
+    parts = line.strip().split(" ")
+    if parts[1] == "cd":
+        if parts[2] == "/":
+            path = ()
+        elif parts[2] == "..":
+            path = path[:-1]
+        else:
+            path += (parts[2],)
+        line = next(sys.stdin, None)
+    elif parts[1] == "ls":
+        while True:
+            line = next(sys.stdin, None)
+            if not line or line.startswith("$"):
+                break
+            detail, name = line.split(" ")
+            if detail == "dir":
+                continue
+            for i in range(len(path) + 1):
+                sizes[path[: len(path) - i]] += int(detail)
+
+print(sum(size for size in sizes.values() if size <= 100000))
