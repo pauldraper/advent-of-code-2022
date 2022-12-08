c3d1bf920af2dc07bc6849d10b9542c3cefc05fc
Paul Draper <paul@helloseer.com>
2022-12-08 05:43:17 +0000

    Day 8

---
+++ problems/day-08/part_1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import sys
+
+grid = {}
+for i, line in enumerate(sys.stdin):
+    for j, v in enumerate(line.strip()):
+        grid[(i, j)] = int(v)
+
+
+def visible(p):
+    start = grid[p]
+    for i in range(len(p)):
+        for k in (-1, 1):
+            p2 = list(p)
+            p2[i] += k
+            while tuple(p2) in grid:
+                if start <= grid[tuple(p2)]:
+                    break
+                p2[i] += k
+            else:
+                return True
+    return False
+
+
+print(sum(map(visible, grid)))
---
+++ problems/day-08/part_2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import sys
+
+grid = {}
+for i, line in enumerate(sys.stdin):
+    for j, v in enumerate(line.strip()):
+        grid[(i, j)] = int(v)
+
+best = 0
+for p, base in grid.items():
+    score = 1
+    for i in range(len(p)):
+        for k in (-1, 1):
+            s = 0
+            p2 = list(p)
+            p2[i] += k
+            while tuple(p2) in grid:
+                s += 1
+                if base <= grid[tuple(p2)]:
+                    break
+                p2[i] += k
+            score *= s
+    best = max(best, score)
+
+print(best)
